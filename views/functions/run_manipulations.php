b9921a72803a851f4f271ca4213d70ea7284618f
Andrew Reynolds <areynold@illinois.edu>
2020-09-21 16:57:38 -0500

    stub manipulations form

--- views/functions/run_manipulations.php
+++ views/functions/run_manipulations.php
@@ -1,7 +1,13 @@
-<?php if (!defined('BASEPATH')) {
-    exit('No direct script access allowed');
-} ?>
+<?php
+    if (!defined('BASEPATH')) 
+    {
+        exit('No direct script access allowed');
+    }
+
+    ee()->load->helper('form');
+?>
 
 <h2>Run Manipulations</h2>
 
 <p>Runs defined image manipulations against previously uploaded images.</p>
+<?= form_open('run_manipulations'); ?>
