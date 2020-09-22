870de4502f0c9fe878fc9686985ad9cd802706cc
Andrew Reynolds <areynold@illinois.edu>
2020-09-22 10:32:00 -0500

    create upload field picker

--- mcp.image_manipulation_runner.php
+++ mcp.image_manipulation_runner.php
@@ -25,4 +25,33 @@ class Image_manipulation_runner_mcp
     {
         return false;
     }
+
+    private function get_upload_destinations() {
+        $destinations = ee('Model')->get('UploadDestination')
+            ->filter('site_id', ee()->config->item('site_id'))
+            ->filter('module_id', 0) // limit selection to user-defined destinations
+            ->all();
+
+        $file_choices = array();
+        foreach ($destinations as $dest) { 
+            $file_choices[$dest->id] = $dest->name;
+        }
+            
+        
+        $upload_field = array(
+            'title' => 'Image Upload Destination',
+            // should be able to use BASE here, but url swaps session token and uri.
+            'desc' => 'Choose an appropriate image gallery from the <a href="/admin.php?cp/files">Files</a> menu.',
+            'fields' => array(
+                'npr_image_destination' => array(
+                    'type' => 'radio',
+                    'choices' => $file_choices,
+                    'value' => ''
+                )
+            ),
+            'required' => true
+        );
+
+        return $upload_field;
+    }
 }
